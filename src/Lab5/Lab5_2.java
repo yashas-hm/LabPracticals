a1da89bfa9d5933d18adf95cf6d4d12268b8c1fa
yashas-hm <yashashm@gmail.com>
2021-02-17 12:21:43 +0530

    lab 5 q2 update

--- src/Lab5/Lab5_2.java
+++ src/Lab5/Lab5_2.java
@@ -10,18 +10,21 @@ package Lab5;
 public class Lab5_2 {
     public static void main(String[] args) {
         A3 b = new B3();
+        A3 a = new A3();
+        a.hello();
+        b.hello();
     }
 }
 
 class A3 {
-    public void hello(String s) {
-        System.out.println(s + ": Hello From");
+    public void hello() {
+        System.out.println( "A3 : Hello From");
     }
 }
 
 class B3 extends A3 {
     @Override
-    public void hello(String s) {
-        System.out.println(s + ": Hello From");
+    public void hello() {
+        System.out.println("B3 : Hello From");
     }
 }
